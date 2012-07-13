c2df61b9204f7d1c06d28879ebaf6287219e394b
Mark Brough <mark@mark-Aspire-5755G.(none)>
2012-07-13 12:42:37 +0100

    Updated theme

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 /**
  * The Header for our theme.
  *
@@ -65,13 +65,26 @@
 	 */
 	wp_head();
 ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-18679634-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed">
 	<header id="branding" role="banner">
 			<hgroup>
-				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php bloginfo('template_url'); ?>/images/dd_large.png" alt="<?php bloginfo( 'name' ); ?>" /></a></span></h1>
+				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="http://developmentdatachallenge.org/wp-content/uploads/2012/07/ddc_white.png" alt="<?php bloginfo( 'name' ); ?>" /></a></span></h1>
 				<!--<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>-->
 			</hgroup>
 
@@ -100,3 +113,4 @@
 
 
 	<div id="main">
+
